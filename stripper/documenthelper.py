bfb45c767177fc7d765e76cb89b775a94372670d
Jesse van den Kieboom <jessevdk@gnome.org>
2011-04-08 23:41:59 +0200

    Port to gedit 3.0

--- stripper/documenthelper.py
+++ stripper/documenthelper.py
@@ -1,26 +1,26 @@
 # -*- coding: utf-8 -*-
 
 #  phpcompletion.py - PHP completion using the completion framework
-#  
+#
 #  Copyright (C) 2009 - Jesse van den Kieboom
 #  Copyright (C) 2009 - Ignacio Casal Quinteiro
-#  
+#
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either version 2 of the License, or
 #  (at your option) any later version.
-#   
+#
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-#   
+#
 #  You should have received a copy of the GNU General Public License
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 59 Temple Place, Suite 330,
 #  Boston, MA 02111-1307, USA.
 
-import gtk
+from gi.repository import Gtk, Gdk
 
 class DocumentHelper:
     def __init__(self, view):
@@ -67,13 +67,14 @@ class DocumentHelper:
         self._connect_buffer()
 
     def on_key_press_event(self, view, event):
-        if event.type != gtk.gdk.KEY_PRESS:
+        if event.type != Gdk.EventType.KEY_PRESS:
             return
 
         state = event.state
         key = event.keyval
 
-        if (key == gtk.keysyms.Return or key == gtk.keysyms.KP_Enter) and not (state & gtk.gdk.SHIFT_MASK):
+        if (key == Gdk.KEY_Return or key == Gdk.KEY_KP_Enter) and \
+            not (state & Gdk.ModifierType.SHIFT_MASK):
             piter = self._buffer.get_iter_at_mark(self._buffer.get_insert())
 
             if not piter.backward_line():
--- stripper/windowhelper.py
+++ stripper/windowhelper.py
@@ -1,53 +1,52 @@
 # -*- coding: utf-8 -*-
 
 #  phpcompletion.py - PHP completion using the completion framework
-#  
+#
 #  Copyright (C) 2009 - Jesse van den Kieboom
 #  Copyright (C) 2009 - Ignacio Casal Quinteiro
-#  
+#
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either version 2 of the License, or
 #  (at your option) any later version.
-#   
+#
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-#   
+#
 #  You should have received a copy of the GNU General Public License
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 59 Temple Place, Suite 330,
 #  Boston, MA 02111-1307, USA.
 
 from documenthelper import DocumentHelper
+from gi.repository import Gedit, GObject
 
-class WindowHelper:
+class WindowHelper(GObject.Object, Gedit.WindowActivatable):
+    window = GObject.property(type=Gedit.Window)
+
     DOCUMENT_DATA_KEY = "StripperPluginDocumentData"
 
-    def __init__(self, plugin, window):
-        self._window = window
-        self._plugin = plugin
+    def __init__(self):
+        GObject.Object.__init__(self)
 
-        for view in self._window.get_views():
+    def do_activate(self):
+        for view in self.window.get_views():
             self.add_document_helper(view)
 
-        self._tab_added_id = self._window.connect('tab-added', self.on_tab_added)
-        self._tab_removed_id = self._window.connect('tab-removed', self.on_tab_removed)
+        self._tab_added_id = self.window.connect('tab-added', self.on_tab_added)
+        self._tab_removed_id = self.window.connect('tab-removed', self.on_tab_removed)
 
-    def deactivate(self):
+    def do_deactivate(self):
         # Remove the provider from all the views
-        for view in self._window.get_views():
+        for view in self.window.get_views():
             self.remove_document_helper(view)
 
-        self._window.disconnect(self._tab_added_id)
-        self._window.disconnect(self._tab_removed_id)
+        self.window.disconnect(self._tab_added_id)
+        self.window.disconnect(self._tab_removed_id)
 
-        self._window = None
-        self._plugin = None
-
-    def update_ui(self):
-        pass
+        self.window = None
 
     def add_document_helper(self, view):
         data = view.get_data(self.DOCUMENT_DATA_KEY)
